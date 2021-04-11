7c9642a7ada205c95702a1f68277fc3bd5505254
Aaditya Sriram <aadisriram@gmail.com>
2021-04-11 00:18:35 +0000

    Add some working TS Code with esbuild

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,16 +1,25 @@
 const path = require("path");
+const { ESBuildPlugin } = require("esbuild-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        loader: "esbuild-loader",
         exclude: /node_modules/,
+        options: {
+          loader: 'tsx',
+          target: 'es2015',
+        },
       },
     ],
   },
+  plugins: [new ESBuildPlugin(), 
+    new HtmlWebpackPlugin({ template: "./templates/index.html" })
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
